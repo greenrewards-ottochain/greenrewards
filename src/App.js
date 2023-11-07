b2daa79257387547de2de883637e5c23a9fcc8fe
Horlarmmy <aladetoheeb5@gmail.com>
2023-11-07 17:12:55 +0100

    Fix wagmi config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import AllRoutes from './config/AllRoutes';
 import '@rainbow-me/rainbowkit/styles.css';
-import { darkTheme, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { Chain, darkTheme, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { polygon } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+
+const ottochain = {
+  id: 0x128, // You can assign a unique identifier as needed
+  name: "Otto Chain",
+  network: "otto", 
+  iconUrl: "https://example.com/icon.svg", // Update the icon URL if necessary
+  iconBackground: "#fff", // Update icon background color if needed
+  nativeCurrency: {
+    decimals: 18, 
+    name: "GNC", // Name of the native currency
+    symbol: "GNC", // Symbol for the native currency
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://gateway.testnet.octopus.network/ottochain/m4k5urt9h33dpbhgsp4lqxemo6naeihz"], 
+    },
+    default: {
+      http: ["https://gateway.testnet.octopus.network/ottochain/m4k5urt9h33dpbhgsp4lqxemo6naeihz"], 
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "OTTO",
+      url: "http://34.69.4.240:4000/", 
+    },
+  },
+  contracts: {
+    // Add contract addresses if needed
+  },
+  testnet: true, 
+};
+
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
     // mainnet,
     polygon,
-    //polygonMumbai,
-    // optimism,
-    // arbitrum,
-    // goerli,
-    // ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    ottochain
   ],
   [publicProvider()]
 );
